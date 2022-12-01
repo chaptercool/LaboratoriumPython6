53b40a2d0d02031e6012d0350940acbd5c17da24
w68141 <yr.hvrn15@gmail.com>
2022-12-01 12:20:16 +0100

    v2

--- 1.py
+++ 1.py
@@ -1,5 +1,15 @@
 def document(imie, wiek):
+    ''' Funkcja wykonywa wypisywanie imie oraz wiek.
+    :param imie:
+    :param wiek:
+    :return:
+    '''
     print(imie, wiek)
+
 document("Karolina", 20)
 document("Patryk", 98)
 document(wiek=72, imie="Karolina")
+print(document.__doc__)
+
+print(help(document))
+document("Karolina")
